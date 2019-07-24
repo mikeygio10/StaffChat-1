2b5ae799f0996f193984344d90434581169565f4
anirudh246 <51390305+anirudh246@users.noreply.github.com>
2019-07-24 09:30:37 -0500

    Update main.php

--- src/anirudh246/staffchat/main.php
+++ src/anirudh246/staffchat/main.php
@@ -1,1 +1,33 @@
-
+  
+<?php
+namespace anirudh246/StaffChat
+use _64FF00\PurePerms\PPGroup; 
+use pocketmine/plugin/PluginBase
+use pocketmine/event/Listener
+use pocketmine/event/player/PlayerChatEvent
+use pocketmine/utils/TextFormat
+use pocketmine/command/Command
+use pocketmine/command/CommandSender
+  
+  class main extends PluginBase implements Listener
+  {
+public $chatMessage = array()
+public function onLoad(){
+		$this->getLogger()->info("Plugin Loaded!");
+	}
+	public function onEnable(){
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+		$this->getServer()->getScheduler()->scheduleRepeatingTask(new BroadcastPluginTask($this), 120);
+		$this->getLogger()->info("Plugin Enabled!");
+    }
+	public function onDisable(){
+		$this->getLogger()->info("Plugin Disabled!");
+	}
+  public function staffMessage(){
+  if {($sender-> hasPermission("hgk.staffchat.send"))
+      
+  }
+  else
+  }
+        
+}
