0c5d069def133cde505ecee94f45443d718a56cb
anirudh246 <51390305+anirudh246@users.noreply.github.com>
2019-07-24 07:24:27 -0500

    Create main.php

---
+++ main.php
@@ -0,0 +1,9 @@
+<?php
+namespace anirudh246/StaffChat
+
+use pocketmine/plugin/PluginBase
+use pocketmine/event/Listener
+use pocketmine/event/player/PlayerChatEvent
+use pocketmine/utils/TextFormat
+use pocketmine/command/Command
+use pocketmine/command/CommandSender
